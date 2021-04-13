7f7d3b1c7727e2c20529a63920906e1371e4d874
Ryan de Fost <ryandefost@hotmail.com>
2021-04-13 12:42:14 +0200

    Font veranderd, Quit button, background particals and gamebackground collor to black

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
        SceneManager.LoadScene("BeroepsOpdrachtArcadeMechanics");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
